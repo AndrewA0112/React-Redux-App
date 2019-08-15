a5bc8330e67424451b07b500777761c3c253bb19
Andrew <andrew.mkal@outlook.com>
2019-08-15 18:02:58 -0500

    Styling changes

--- async_redux/src/components/Profile.js
+++ async_redux/src/components/Profile.js
@@ -7,13 +7,11 @@ import './Profile.scss'
 
 const Profile = ({ isLoading, profile, getProfile }) => {
 
-    // const { isLoading, profile } = props
-    // console.log('PROPS', props)
-
-    const [profileInput, setProfileInput] = useState('Gooddog228')
+    const [profileInput, setProfileInput] = useState('')
 
     const getProfileDispatch = () => {
         getProfile(profile, profileInput)
+        setProfileInput('')
     }
 
     const handleChange = e => {
@@ -25,7 +23,11 @@ const Profile = ({ isLoading, profile, getProfile }) => {
             <>
                 <input value={profileInput} onChange={handleChange} />
                 <button onClick={getProfileDispatch}>Fetch Profile</button>
-                <h1>No Profile Found</h1>
+                {isLoading ? (
+                    <Loader type="ThreeDots" color="black" height={80} width={80} style={{margin: 50 + 'px'}}/>
+                ) : (
+                    <h1>No Profile Found</h1>
+                )}
             </>
         )
     } else {
@@ -34,7 +36,7 @@ const Profile = ({ isLoading, profile, getProfile }) => {
                 <input value={profileInput} onChange={handleChange} />
                 <button onClick={getProfileDispatch}>Fetch Profile</button>
                 {isLoading ? (
-                    <Loader type="TailSpin" color="black" height="100" width="100" />
+                    <Loader type="TailSpin"/>
                 ) : (
                     <div>
                         <h1>{profile.player.name}</h1>
@@ -42,9 +44,9 @@ const Profile = ({ isLoading, profile, getProfile }) => {
                         <h3>Time Played: {parseFloat(Math.round(profile.player.timePlayed / 3600 * 100) / 100).toFixed(2)} Hours</h3>
                         <div>
                             <h3>K/D: {parseFloat(profile.stats.extra.kdr).toFixed(2)}</h3>
-                            <h3>Total Score: {profile.stats.scores.score}</h3>
-                            <h3>Conquest Score: {profile.stats.modes[0].score}</h3>
-                            <h3>Full Stats: <a href={profile.player.blPlayer} target='_blank'>{profile.player.name}'s Stats</a></h3>
+                            <h3>Total Score: {profile.stats.scores.score.toLocaleString()}</h3>
+                            <h3>Conquest Score: {profile.stats.modes[0].score.toLocaleString()}</h3>
+                            <h3>Full Stats: <a href={profile.player.blPlayer} target='_blank' rel="noopener noreferrer">{profile.player.name}'s Stats</a></h3>
                         </div>
                     </div>
                 )}
